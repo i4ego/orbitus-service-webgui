6b69145cf4a39ca05cb26dfac633c91e60ebb300
i4ego <me@i4ego.ru>
2026-09-18 23:09:47 +0500

    update version

--- service.bat
+++ service.bat
@@ -1,5 +1,5 @@
 @echo off
-set "LOCAL_VERSION=1.10.2"
+set "LOCAL_VERSION=ui 0.1 | orbitus-service 1.10.2"
 
 :: External commands
 if "%~1"=="status_zapret" (
@@ -371,9 +371,9 @@ chcp 437 > nul
 cls
 
 :: Set current version and URLs
-set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/main/.service/version.txt"
-set "GITHUB_RELEASE_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/tag/"
-set "GITHUB_DOWNLOAD_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/latest"
+set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/i4ego/orbitus-service-webgui/main/.service/version.txt"
+set "GITHUB_RELEASE_URL=https://github.com/i4ego/orbitus-service-webgui/releases/tag/"
+set "GITHUB_DOWNLOAD_URL=https://github.com/i4ego/orbitus-service-webgui/releases/latest"
 
 :: Get the latest version from GitHub
 for /f "delims=" %%A in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri \"%GITHUB_VERSION_URL%\" -Headers @{\"Cache-Control\"=\"no-cache\"} -UseBasicParsing -TimeoutSec 5).Content.Trim()" 2^>nul') do set "GITHUB_VERSION=%%A"
